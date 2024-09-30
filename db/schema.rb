68c74e8ecd226f5dda9ab26e8515e8514c93a542
Josh Newton <Joshnewton1999@gmail.com>
2024-09-30 11:04:52 +0100

    added validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_30_085607) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_30_094311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.string "comment"
+    t.text "comment"
+    t.bigint "recipe_id", null: false
+    t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_bookmarks_on_category_id"
+    t.index ["recipe_id"], name: "index_bookmarks_on_recipe_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -34,4 +38,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_30_085607) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "bookmarks", "categories"
+  add_foreign_key "bookmarks", "recipes"
 end
